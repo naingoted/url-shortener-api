18e2de2d6659ed7218800f9ebdbb3fdadde211b3
vincent.thet <naingoted@gmail.com>
2023-10-22 19:26:14 +0800

    test deployment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.APP_PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
