7efbc3766db7d81954e0ca117f2816114e063b89
vincent.thet <naingoted@gmail.com>
2023-10-22 19:37:58 +0800

    fix deployment

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.APP_PORT || 4000;
+  const port = process.env.PORT || 4000;
   await app.listen(port, "0.0.0.0");
 }
 bootstrap();
