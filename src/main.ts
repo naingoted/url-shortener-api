3d4763560398a7e5765627b73c6d3236f7a5b591
vincent.thet <naingoted@gmail.com>
2023-10-23 03:06:10 +0800

    updated cors settings

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: ["https://url-shortener-react-dusky.vercel.app"], // specify the allowed domain
+    methods: "GET,POST",
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 4000;
   await app.listen(port, "0.0.0.0");
